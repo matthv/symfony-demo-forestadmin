d34871297ff3d44ee3e1cafca5629f577b0ad424
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2022-10-13 10:54:08 +0200

    chore: update import

--- config/packages/symfony_forest_admin.php
+++ config/packages/symfony_forest_admin.php
@@ -1,11 +1,9 @@
 <?php
 
-use ForestAdmin\AgentPHP\DatasourceDoctrine\Datasource;
+use ForestAdmin\AgentPHP\DatasourceDoctrine\DoctrineDatasource;
 use Nicolas\SymfonyForestAdmin\Service\ForestAgent;
 
 return static function (ForestAgent $forestAgent) {
-    $forestAgent->addDatasource(new Datasource($forestAgent->getEntityManager()))
-        ->addDatasource(new ForestAdmin\AgentPHP\DatasourceDummy\DummyDatasource())
-        ->customizeCollection('Book', BookCustomize::custom($forestAgent))
-    ;
+    $forestAgent->addDatasource(new DoctrineDatasource($forestAgent->getEntityManager()))
+        ->addDatasource(new ForestAdmin\AgentPHP\DatasourceDummy\DummyDatasource());
 };
