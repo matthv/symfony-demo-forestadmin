ab1d34a935adc3c6a4d4fa75e5980bcc777511e8
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2022-10-25 16:32:04 +0200

    chore: update conf

--- config/packages/symfony_forest_admin.php
+++ config/packages/symfony_forest_admin.php
@@ -1,9 +1,33 @@
 <?php
 
+use ForestAdmin\AgentPHP\Agent\Builder\CollectionBuilder;
 use ForestAdmin\AgentPHP\DatasourceDoctrine\DoctrineDatasource;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Decorators\Computed\ComputedDefinition;
 use ForestAdmin\SymfonyForestAdmin\Service\ForestAgent;
 
 return static function (ForestAgent $forestAgent) {
-    $forestAgent->addDatasource(new DoctrineDatasource($forestAgent->getEntityManager()))
-        ->addDatasource(new ForestAdmin\AgentPHP\DatasourceDummy\DummyDatasource());
+    $forestAgent->agent->addDatasource(new DoctrineDatasource($forestAgent->getEntityManager()))
+        ->addDatasource(new ForestAdmin\AgentPHP\DatasourceDummy\DummyDatasource())
+        ->customizeCollection(
+            'Book',
+            fn (CollectionBuilder $builder) => $builder->addField(
+                'toto',
+                new ComputedDefinition(
+                    columnType: 'String',
+                    dependencies: ["firstname", "lastname"],
+                    values: fn ($records) => $records,
+                )
+            )
+        )
+        ->customizeCollection('Car', function (CollectionBuilder $builder) {
+            $builder->addField(
+                'registrationNumber',
+                new ComputedDefinition(
+                    'String',
+                    ['model'],
+                    fn ($records) => collect($records)->map(fn ($record) => $record['model'] . ' : BP-' . rand(100, 1000) . '-WY')
+                )
+            );
+        })
+        ->build();
 };
