5775cf7d71d22f99e6ac302bcba4f32132409af4
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2022-10-27 15:13:24 +0200

    chore: update config

--- config/packages/symfony_forest_admin.php
+++ config/packages/symfony_forest_admin.php
@@ -2,6 +2,7 @@
 
 use ForestAdmin\AgentPHP\Agent\Builder\CollectionBuilder;
 use ForestAdmin\AgentPHP\DatasourceDoctrine\DoctrineDatasource;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Operators;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Decorators\Computed\ComputedDefinition;
 use ForestAdmin\SymfonyForestAdmin\Service\ForestAgent;
 
@@ -14,7 +15,7 @@ return static function (ForestAgent $forestAgent) {
                 'toto',
                 new ComputedDefinition(
                     columnType: 'String',
-                    dependencies: ["firstname", "lastname"],
+                    dependencies: ["title", "author"],
                     values: fn ($records) => $records,
                 )
             )
@@ -27,6 +28,23 @@ return static function (ForestAgent $forestAgent) {
                     ['model'],
                     fn ($records) => collect($records)->map(fn ($record) => $record['model'] . ' : BP-' . rand(100, 1000) . '-WY')
                 )
+            );
+        })
+        ->customizeCollection(class_basename(User::class), function (CollectionBuilder $builder) {
+            $builder->replaceSearch(
+                fn ($search, $extended) => [
+                    'aggregator' => 'And',
+                    'conditions' => [
+                        ['field' => 'rememberToken', 'operator' => Operators::EQUAL, 'value' => 1],
+                        [
+                            'aggregator' => 'Or',
+                            'conditions' => [
+                                ['field' => 'name', 'operator' => Operators::CONTAINS, 'value' => $search],
+                                ['field' => 'email', 'operator' => Operators::CONTAINS, 'value' => $search],
+                            ],
+                        ]
+                    ],
+                ]
             );
         })
         ->build();
